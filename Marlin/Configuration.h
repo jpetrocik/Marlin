9911307a90f6c44b4f93022254cbfe34e9f3b778
John Petrocik <john@petrocik.net>
2018-05-07 20:40:20 -0700

    Changed z steps from 2560 to 2860

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(Marlin 1.1.x, 20171121)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Marlin 1.1.20180506, John Petrocik)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -525,7 +525,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 50, 50, 2560, 886 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 50, 50, 2860, 886 }
 
 /**
  * Default Max Feed Rate (mm/s)
